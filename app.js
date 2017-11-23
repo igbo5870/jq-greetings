f49e2f1d6daa85b130f2a8e3722b4e0bb721281a
Grégory <gregory.bouloc@gmail.com>
2017-11-23 21:00:32 +0100

    Une solution parmis tant d'autre

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('have fun !');
+console.log(" ");
+var object = {
+}
+
+
+object['firstName'] =  $("input#first_name").val();
+object['lastName'] = $("input#last_name").val();
+object['ville'] = $("input#city").val();
+
+console.log(object);
+
+if(object.firstName && object.lastName !== ""){
+	$("#username").replaceWith(object.firstName + " "+ object.lastName + " de " + object.ville);
+}
